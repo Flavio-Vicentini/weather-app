5d976f685a2595d2c81f1ca589f618e74185f0f1
Flavio Vicentini <flaviovicentinijr@gmail.com>
2021-02-12 20:04:42 -0300

    remove unecessary console log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('js client is loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const mainContent = document.querySelector('.main-content')
